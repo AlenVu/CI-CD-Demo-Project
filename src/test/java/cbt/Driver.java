489123634c3710c6faf9b3ff61f4d43276ccb1cf
AlenVu <alen.jakumetovic@gmail.com>
2023-03-02 13:06:17 +0100

    update

--- src/test/java/cbt/Driver.java
+++ src/test/java/cbt/Driver.java
@@ -35,7 +35,7 @@ public class Driver {
                 case "remote-chrome":
                     try {
                         // assign your grid server address
-                        String gridAddress = "100.25.41.122";
+                        String gridAddress = "18.235.243.146";
                         URL url = new URL("http://" + gridAddress + ":4444/wd/hub");
                         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
                         desiredCapabilities.setBrowserName("chrome");
